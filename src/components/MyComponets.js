baaa2c0aa1795b8bb660ba904fd3e1d469d9aab4
kled51021 <kled51021@gmail.com>
2024-01-10 11:05:11 +0700

    Form với React

--- src/components/MyComponets.js
+++ src/components/MyComponets.js
@@ -1,37 +1,30 @@
-// class component
-// function component
 import React from "react";
 class MyComponent extends React.Component {
     state = {
+        name: "HaryPhamDev",
         name: "HaryPhamDev ",
         address: "Hoi dan It",
         age: 26
     };
-    handleclick = (event) => {
-        //console.log(">>>> Click me my button");
-        console.log("My name is , ", this.state.name)
-        console.log(" random ", Math.floor((Math.random() * 100) + 1))
-        // merge State => React class
-        this.setState( // update name
-            {
-                name: 'Eric ',
-                age: (Math.floor((Math.random() * 100) + 1))
-            }
-        )
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
     }
-    handleOnMouseOver(event) {
-        console.log(this.state.address)
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
     }
     //JSX -- có thể viết code js trong html
     render() {
         return (
-
             <div>
                 My name is {this.state.name}
                 I'm  {this.state.age}
-                <button onClick={(event) => { this.handleclick(event) }}>Click me</button>
-                <button onMouseOver={(event) => { this.handleOnMouseOver(event) }}>Hover me</button>
-
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input type="text" onChange={(event) => this.handleOnChange(event)}></input>
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
