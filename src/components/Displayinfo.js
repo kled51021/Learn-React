375ae620919b6cd7d67067bc6ebfed48caf8252a
kled51021 <kled51021@gmail.com>
2024-01-14 13:24:26 +0700

    React cyclelife

--- src/components/Displayinfo.js
+++ src/components/Displayinfo.js
@@ -2,15 +2,37 @@ import React from "react";
 import './Displayinfo.scss';
 import logo from './../logo.svg'
 class Displayinfo extends React.Component {
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log("call constrcutor : 1")
+        super(props)
+        this.state = {
+            isShowListUser: true
+        }
     }
+    componentDidMount() {
+        console.log('>>> call me component did mount')
+        setTimeout(() => {
+            document.title = "Hello"
+        }, 3000);
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(">>> call me component did update", this.props, prevProps)
+        if (this.props.ListUser !== prevProps.ListUser) {
+            if (this.props.ListUser.length === 5) {
+                alert("You got 5 user")
+            }
+        }
+
+
+    }
+
     ShowHide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser
         })
     }
     render() {
+        console.log(">> call me render")
         const { ListUser } = this.props;
         return (
             <div className="display-info-container">
