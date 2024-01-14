c90a6c66e6bd4d74a72af02eb4c3619ce7ba6446
kled51021 <kled51021@gmail.com>
2024-01-14 13:37:07 +0700

    React StateFul/StateLess Component

--- src/components/Displayinfo.js
+++ src/components/Displayinfo.js
@@ -1,48 +1,14 @@
 import React from "react";
 import './Displayinfo.scss';
 import logo from './../logo.svg'
-class Displayinfo extends React.Component {
-    constructor(props) {
-        console.log("call constrcutor : 1")
-        super(props)
-        this.state = {
-            isShowListUser: true
-        }
-    }
-    componentDidMount() {
-        console.log('>>> call me component did mount')
-        setTimeout(() => {
-            document.title = "Hello"
-        }, 3000);
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log(">>> call me component did update", this.props, prevProps)
-        if (this.props.ListUser !== prevProps.ListUser) {
-            if (this.props.ListUser.length === 5) {
-                alert("You got 5 user")
-            }
-        }
-
-
-    }
-
-    ShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
+/* class Displayinfo extends React.Component {
     render() {
         console.log(">> call me render")
         const { ListUser } = this.props;
         return (
             <div className="display-info-container">
-                <img src={logo}></img>
-                <div>
-                    <button onClick={() => { this.ShowHide() }}>
-                        {this.state.isShowListUser === true ? "Hide" : "Show"}
-                    </button>
-                </div>
-                {this.state.isShowListUser &&
+
+                {true &&
                     <div>
                         {ListUser.map((User, index) => {
                             return (
@@ -59,5 +25,29 @@ class Displayinfo extends React.Component {
             </div>
         )
     }
+} */
+const Displayinfo = (props) => {
+    const { ListUser } = props;
+    return (
+        <div className="display-info-container" >
+
+            {true &&
+                <div>
+                    {ListUser.map((User, index) => {
+                        return (
+                            <div key={User.id} className={+User.age > 18 ? "green" : "red"}>
+                                <div>My name's {User.name}</div>
+                                <div>My Age's {User.age}</div>
+                                <button onClick={() => props.HandleDeleteUser(User.id)}>Detele</button>
+                                <hr></hr>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+
+        </div >
+
+    )
 }
 export default Displayinfo;
