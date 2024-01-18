31d3a0d54ec2ddf32c08dd8d0cb681e7d58ae0ba
kled51021 <kled51021@gmail.com>
2024-01-18 23:52:53 +0700

    Dieu huong trang voi Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
+import { Button } from 'bootstrap';
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 const App = () => {
   return (
     <div className='App-container'>
-      <Header></Header>
+      <Header />
+      <div>
+        Test Link
+      </div>
+      <div>
+        <button>
+          <Link to="/users">go to User </Link>
+        </button>
+        <button>
+          <Link to="/admins">go to Admin </Link>
+        </button>
+      </div>
     </div>
   )
 }
